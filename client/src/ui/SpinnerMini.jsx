b783955acdc8436e26dc0232955a236456c8735d
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-11-23 00:36:18 +0530

    Add Some ui functionality 1

--- client/src/ui/SpinnerMini.jsx
+++ client/src/ui/SpinnerMini.jsx
@@ -1,8 +1,10 @@
 import { Spinner } from '@material-tailwind/react';
 function SpinnerMini() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <Spinner color="blue" className="h-5 w-5" />
+    <div className="relative min-h-screen w-full flex-1 bg-green-100 p-[8rem_1rem] font-sans tracking-wide dark:bg-slate-900 md:p-[8rem_6rem]">
+      <div className="flex h-full w-full items-center justify-center">
+        <Spinner color="blue" className="h-5 w-5" />
+      </div>
     </div>
   );
 }
