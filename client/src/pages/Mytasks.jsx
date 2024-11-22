4ac01288e00e7b6dc70bdcd9415121a876edcf6e
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-11-23 00:33:41 +0530

    Add Some ui functionality 1

--- client/src/pages/Mytasks.jsx
+++ client/src/pages/Mytasks.jsx
@@ -18,27 +18,31 @@ function Mytasks() {
 
   if (isLoading) {
     return (
-      <div className="mt-10 flex flex-col items-center">
-        <h2 className="text-xl font-semibold text-gray-700">
-          Loading Your Tasks
-        </h2>
-        <p className="text-sm text-gray-500">
-          Please wait while we fetch your tasks...
-        </p>
-        <div className="mt-5 h-10 w-10 animate-spin rounded-full border-4 border-green-500 border-t-transparent"></div>
+      <div className="relative min-h-screen w-full flex-1 bg-green-100 p-[8rem_1rem] font-sans tracking-wide dark:bg-slate-900 md:p-[8rem_6rem]">
+        <div className="mt-10 flex flex-col items-center">
+          <h2 className="text-xl font-semibold text-gray-700">
+            Loading Your Tasks
+          </h2>
+          <p className="text-sm text-gray-500">
+            Please wait while we fetch your tasks...
+          </p>
+          <div className="mt-5 h-10 w-10 animate-spin rounded-full border-4 border-green-500 border-t-transparent"></div>
+        </div>
       </div>
     );
   }
 
   if (isError) {
     return (
-      <div className="mt-10 text-center">
-        <h2 className="text-xl font-semibold text-red-600">
-          Error Loading Tasks
-        </h2>
-        <p className="text-sm text-gray-500">
-          Something went wrong. Please try again later.
-        </p>
+      <div className="relative min-h-screen w-full flex-1 bg-green-100 p-[8rem_1rem] font-sans tracking-wide dark:bg-slate-900 md:p-[8rem_6rem]">
+        <div className="mt-10 text-center">
+          <h2 className="text-xl font-semibold text-red-600">
+            Error Loading Tasks
+          </h2>
+          <p className="text-sm text-gray-500">
+            Something went wrong. Please try again later.
+          </p>
+        </div>
       </div>
     );
   }
