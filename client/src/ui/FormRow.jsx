c88bf1c188fc3e01596ed584c0a5dae1966d882d
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-12-25 20:31:40 +0530

    Add About task

--- client/src/ui/FormRow.jsx
+++ client/src/ui/FormRow.jsx
@@ -1,11 +1,10 @@
 function FormRow({ label, error, children, icon }) {
   return (
     <div className="flex-col border-b px-4 py-3 dark:border-slate-600 md:px-0">
-      <div className="items-center gap-[3.5rem] md:grid md:grid-cols-[10rem_1fr_1fr]">
+      <div className="items-center gap-[3.5rem] md:grid md:grid-cols-[10rem_1fr]">
         {label && (
           <div className="flex items-center">
             {icon && icon}
-            {/* <AiOutlineCalendar className="mr-2 text-2xl text-green-500" /> */}
             <label
               htmlFor={children.props.id}
               className="font-sans text-sm font-semibold tracking-widest text-stone-900 dark:text-grey-100 md:text-base"
@@ -14,7 +13,8 @@ function FormRow({ label, error, children, icon }) {
             </label>
           </div>
         )}
-        {children}
+
+        <div className="md:ml-auto">{children}</div>
       </div>
       {error && (
         <span className="text-sm tracking-wider text-red-700">💥 {error}</span>
