31c77440e2d079972920c2b9ca49e1f8f91a62cf
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-12-25 20:25:44 +0530

    Add About task

--- client/src/components/activities/GetFullDetailTask.jsx
+++ client/src/components/activities/GetFullDetailTask.jsx
@@ -80,53 +80,68 @@ function GetFullDetailTask({
         />
       </FormRow>
 
-      <FormRow label="Submission Link">
-        <a
-          href={defaultTaskValues.googleDriveLink || '#'}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="dark:text-grey-400mt-2 inline-block w-[12rem] rounded-md border-[1px] border-solid border-grey-300 bg-transparent p-[0.4rem_0.8rem] text-sm tracking-wider text-blue-600 shadow-sm hover:text-blue-800 dark:border-slate-600 dark:text-blue-400 dark:hover:text-blue-600"
-        >
-          {defaultTaskValues.googleDriveLink || 'No link provided'}
-        </a>
-      </FormRow>
+      {defaultTaskValues.submittedTaskDesc ||
+      defaultTaskValues.googleDriveLink ? (
+        <>
+          <FormRow label="Submission Link">
+            <a
+              href={defaultTaskValues.googleDriveLink || '#'}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="dark:text-grey-400mt-2 inline-block w-[12rem] rounded-md border-[1px] border-solid border-grey-300 bg-transparent p-[0.4rem_0.8rem] text-sm tracking-wider text-blue-600 shadow-sm hover:text-blue-800 dark:border-slate-600 dark:text-blue-400 dark:hover:text-blue-600"
+            >
+              {defaultTaskValues.googleDriveLink || 'No link provided'}
+            </a>
+          </FormRow>
 
-      <FormRow label="Submission Description">
-        <textarea
-          className="h-[5rem] w-[12rem] rounded-md border-[1px] border-solid border-grey-300 bg-transparent p-[0.4rem_0.8rem] text-sm tracking-wider shadow-sm dark:border-slate-600 dark:text-grey-400"
-          disabled
-          value={
-            defaultTaskValues.submittedTaskDesc || 'No description provided'
-          }
-        />
-      </FormRow>
+          <FormRow label="Submission Description">
+            <textarea
+              className="h-[5rem] w-[12rem] rounded-md border-[1px] border-solid border-grey-300 bg-transparent p-[0.4rem_0.8rem] text-sm tracking-wider shadow-sm dark:border-slate-600 dark:text-grey-400"
+              disabled
+              value={
+                defaultTaskValues.submittedTaskDesc || 'No description provided'
+              }
+            />
+          </FormRow>
 
-      <FormRow label="Submitted Date">
-        <input
-          className="w-[12rem] rounded-md border-[1px] border-solid border-grey-300 bg-transparent p-[0.4rem_0.8rem] text-sm tracking-wider shadow-sm dark:border-slate-600 dark:text-grey-400"
-          type="text"
-          disabled
-          value={defaultTaskValues.submitDate || 'N/A'}
-        />
-      </FormRow>
+          <FormRow label="Submitted Date">
+            <input
+              className="w-[12rem] rounded-md border-[1px] border-solid border-grey-300 bg-transparent p-[0.4rem_0.8rem] text-sm tracking-wider shadow-sm dark:border-slate-600 dark:text-grey-400"
+              type="text"
+              disabled
+              value={defaultTaskValues.submitDate || 'N/A'}
+            />
+          </FormRow>
 
-      <div className="m-auto flex items-center justify-center gap-9 p-[0.5rem_0]">
-        <Button
-          type="reset"
-          disabled={disabled || isReviewing}
-          onClick={() => handleReview('rejected')}
-        >
-          <FaTimesCircle className="mr-2" /> Reject
-        </Button>
-        <Button
-          type="danger"
-          disabled={disabled || isReviewing}
-          onClick={() => handleReview('accepted')}
-        >
-          <FaCheckCircle className="mr-2" />
-          Accept
-        </Button>
-      </div>
+          <div className="m-auto flex items-center justify-center gap-9 p-[0.5rem_0]">
+            <Button
+              type="reset"
+              disabled={disabled || isReviewing}
+              onClick={() => handleReview('rejected')}
+            >
+              <FaTimesCircle className="mr-2" /> Reject
+            </Button>
+            <Button
+              type="danger"
+              disabled={disabled || isReviewing}
+              onClick={() => handleReview('accepted')}
+            >
+              <FaCheckCircle className="mr-2" />
+              Accept
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div className="rounded-md border border-dashed border-gray-300 bg-yellow-50 p-4 text-center shadow-md dark:border-yellow-600 dark:bg-yellow-900">
+          <p className="mb-2 text-xl font-semibold text-yellow-800 dark:text-yellow-300">
+            Task Not Submitted
+          </p>
+          <p className="text-sm text-yellow-700 dark:text-yellow-400">
+            The user has not submitted this task yet. Please follow up if
+            necessary.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
